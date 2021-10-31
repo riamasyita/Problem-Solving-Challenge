2a6e051bfaa18935627ded7e0de210c07d56743f
Ria Masyita <70877011+riamasyita@users.noreply.github.com>
2021-10-31 22:50:45 +0700

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+ 
+    [Header("Ball Movement")]
+    public float speed = 6f; 
+
+    Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    public void Move(float h, float v)
+    {
+        Vector3 temVect = new Vector3(h, v, 0);
+        temVect = temVect.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.transform.position + temVect);
+    }
+
+}
